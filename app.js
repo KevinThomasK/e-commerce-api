91b7e3efdee171c787938bea2a8b07c3d7cbc906
kevin thomas <kevinthomas0420@gmail.com>
2024-01-27 23:48:16 +0530

    added few fixes

--- app.js
+++ app.js
@@ -38,23 +38,12 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
-app.use(morgan("tiny"));
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static("./public"));
 app.use(fileUpload());
-
-app.get("/", (req, res) => {
-  console.log(req.cookies);
-  res.send("e-commerce api");
-});
-
-app.get("/api/v1", (req, res) => {
-  //console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.send("e-commerce api");
-});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
